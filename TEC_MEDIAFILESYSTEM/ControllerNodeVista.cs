d946c07c4e53fd5cc63cb0385ba575a500e5529d
SRZpeedTEC <s.boza.1@estudiantec.cr>
2025-06-06 22:32:12 -0600

    Añadir solución y proyectos dentro del repo raíz

---
+++ TEC_MEDIAFILESYSTEM/ControllerNodeVista.cs
@@ -0,0 +1,10 @@
+namespace TEC_MEDIAFILESYSTEM
+{
+    public partial class ControllerNodeVista : Form
+    {
+        public ControllerNodeVista()
+        {
+            InitializeComponent();
+        }
+    }
+}
