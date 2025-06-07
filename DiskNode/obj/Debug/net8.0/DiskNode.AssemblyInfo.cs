fd11bf5681226b2d95c6e25014acd9c8c5e5137d
SRZpeedTEC <s.boza.1@estudiantec.cr>
2025-06-06 21:52:20 -0600

    pequeno error

--- DiskNode/obj/Debug/net8.0/DiskNode.AssemblyInfo.cs
+++ DiskNode/obj/Debug/net8.0/DiskNode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabee6289c4452f7bffb638127abed52fa7dd760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102d910e6502d6297595d813948e5bce20767634")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
