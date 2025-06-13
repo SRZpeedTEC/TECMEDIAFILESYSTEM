5408e8dd139bd8a1a4cc9a7ca5e32b21ce6f85de
Rachel <166075283+PFRachel@users.noreply.github.com>
2025-06-12 19:52:04 -0600

    inicioInterfaz

--- DiskNode/obj/Debug/net8.0/DiskNode.AssemblyInfo.cs
+++ DiskNode/obj/Debug/net8.0/DiskNode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccd2688f38b87ee6f6b97f73df6bae46ada8cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f587849307379f838da09b6632fdb41ed4a2fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiskNode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
