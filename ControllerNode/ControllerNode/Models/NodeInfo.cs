5ead7e9bbc6f7567a00448958cec63cda62e5ed7
SRZpeedTEC <s.boza.1@estudiantec.cr>
2025-06-19 00:30:38 -0600

    wasaaaaaa

---
+++ ControllerNode/ControllerNode/Models/NodeInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerNode.Models
+{
+    public record NodeInfo(int Id, string Url);
+}
---
+++ DiskNode3/Services/BlockStorage.cs
@@ -0,0 +1,62 @@
+﻿namespace DiskNode.Services;
+using System.Xml;
+
+
+
+
+public class BlockStorage
+{
+
+    // ATRIBUTES
+    private readonly string path;
+    private readonly int blockSize;
+    private readonly int blockLimit;
+
+    // GETTERS
+    public int GetBlockSize() => blockSize;      
+
+    public BlockStorage(IConfiguration config)      // Constructor that initializes the storage path and block size from configuration
+    {
+        path = config["DiskNode:Path"];
+        var totalSize = int.Parse(config["DiskNode:TotalSizeMB"]) * 1024;
+
+        this.blockSize = int.Parse(config["DiskNode:BlockSizeKB"]) * 1024;
+        this.blockLimit = totalSize / blockSize;
+
+        Directory.CreateDirectory(path);
+    }
+
+    private string GetBlockPath(long index)=> Path.Combine(path, $"block_{index:D8}.bin");
+
+    public async Task WriteAsync(long index, Stream source, CancellationToken cancellationToken)        // Method to write a block of data to the storage
+    {
+        if (index >= blockLimit)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "The limit was reached");          
+        }
+
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "The index can't be negative");
+        }
+
+        await using var fileToSeek = new FileStream(GetBlockPath(index), FileMode.Create, FileAccess.Write, FileShare.None, 81920, true);
+
+        cancellationToken.ThrowIfCancellationRequested();
+        await source.CopyToAsync(fileToSeek, 81920, cancellationToken);
+
+    }
+
+    public async Task<FileStream> ReadAsync(long index, CancellationToken cancellationToken)     // Methot to read a block of data from the storage
+    {
+        var blockPath = GetBlockPath(index);
+
+        if (!File.Exists(blockPath))
+        {
+            throw new FileNotFoundException("Archivo no encontrado", blockPath); 
+        }
+        return new FileStream(blockPath, FileMode.Open, FileAccess.Read, FileShare.Read, 81920, true);
+    }
+
+}
+
